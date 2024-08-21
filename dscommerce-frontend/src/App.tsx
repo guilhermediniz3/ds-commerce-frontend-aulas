4711910be977341905e2f69a3b04f538fae808cb
guilhermediniz3 <gdiniztec@yahoo.com.br>
2024-08-21 15:47:15 -0300

    Criando outros componentes da página de detalhes

--- dscommerce-frontend/src/App.tsx
+++ dscommerce-frontend/src/App.tsx
@@ -1,10 +1,25 @@
-
 import './App.css'
+
+import ButtonInverse from './components/ButtonInverse';
+import ButtonPrimary from './components/ButtonPrimary'
+import HeaderCliente from './components/HeaderClient'
+import ProductDetailsCard from './components/ProductDetailsCard';
 
 function App() {
   return (
-<h1>DSCommerce</h1>
-
+    <>
+      <HeaderCliente />
+      <main>
+        <section id="product-details-section" className="dsc-container">
+        <ProductDetailsCard/>
+ 
+          <div className="dsc-btn-page-container">
+           <ButtonPrimary />
+           <ButtonInverse/>
+          </div>
+        </section>
+      </main>
+    </>
   );
 }
 

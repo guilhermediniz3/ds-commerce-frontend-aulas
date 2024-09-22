654085d7612d9da81f6157f83be8cfbe9c11d928
guilhermediniz3 <gdiniztec@yahoo.com.br>
2024-09-22 19:21:35 -0300

    Renderizando catálogo de produtos dinamicamente

--- dscommerce-frontend/src/routes/ClientHome/Catalog/index.tsx
+++ dscommerce-frontend/src/routes/ClientHome/Catalog/index.tsx
@@ -4,32 +4,11 @@ import './style.css'
 import SearchBar from '../../../components/SearchBar';
 import CatalogCard from '../../../components/CatalogCard';
 import ButtonNextPage from '../../../components/ButtonNextPage';
-import { ProductDTO } from '../../../models/product';
+import * as productService from '../../../services/product-service';
 
-const product: ProductDTO = {
-    id: 2,
-    name: "Smart TV",
-    description: "TV 50 polegadas",
-    imgUrl: "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg",
-    price: 2500.99,
-    categories: [
-        {
-            id: 2,
-            name: "Eletronicos"
-        },
-        {
-            id: 3,
-            name: "Computadores"
-        },
-        {
-            id: 4,
-            name: "Importados"
 
-        }
 
-    ]
 
-}
 
 
 export default function Catalog() {
@@ -42,18 +21,10 @@ export default function Catalog() {
 
                 <SearchBar />
                 <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
-                    <CatalogCard product={product} />
-                    <CatalogCard product={product} />
-                    <CatalogCard product={product} />
-                    <CatalogCard product={product} />
-                    <CatalogCard product={product} />
-                    <CatalogCard product={product} />
-                    <CatalogCard product={product} />
-                    <CatalogCard product={product} />
-                    <CatalogCard product={product} />
-                    <CatalogCard product={product} />
-                    <CatalogCard product={product} />
-                    <CatalogCard product={product} />
+                    {
+                        productService.findAll().map(product =><CatalogCard key={product.id} product={product} /> )
+                    }
+                  
 
                 </div>
                 <ButtonNextPage />
